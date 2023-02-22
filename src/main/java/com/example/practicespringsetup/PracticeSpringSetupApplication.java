743716fdd9dcb72f712764c700da7fa7b805734e
lineisha gibson <lineishagibson@gmail.com>
2023-02-22 11:52:07 -0600

    .

---
+++ src/main/java/com/example/practicespringsetup/PracticeSpringSetupApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicespringsetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringSetupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSpringSetupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practicespringsetup/PracticeSpringSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicespringsetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringSetupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
